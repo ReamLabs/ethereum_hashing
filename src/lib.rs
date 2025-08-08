2649fd33ec99a4a4c556cc877990d018594d57df
Varun Doshi <doshivarun202@gmail.com>
2025-08-08 13:08:30 +0530

    chore: export all functions (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg(feature = "zkvm")]
 mod zkvm_impl;
 #[cfg(feature = "zkvm")]
-pub use zkvm_impl::hash_fixed;
+pub use zkvm_impl::*;
 
 #[cfg(not(feature = "zkvm"))]
 mod dynamic_impl;
 #[cfg(not(feature = "zkvm"))]
-pub use dynamic_impl::hash_fixed;
+pub use dynamic_impl::*;
